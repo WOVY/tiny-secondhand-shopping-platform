1d2eded4878c1a4fb89dc58c01cc004bde7d7802
WOVY <wovy0601@gmail.com>
2026-07-08 12:52:16 +0900

    회원가입/로그인/로그아웃/마이페이지 + 세션/CSRF 적용

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 const path = require('path');
 const express = require('express');
 const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const { doubleCsrf } = require('csrf-csrf');
+
+const config = require('./config/env');
+const userModel = require('./models/user');
 
 const app = express();
 
@@ -12,6 +18,50 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
+app.use(cookieParser());
+app.use(
+  session({
+    secret: config.sessionSecret,
+    resave: false,
+    // CSRF 토큰이 세션 id에 묶이므로, 세션 쿠키가 로그인 전에도 발급되어야 함
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+      sameSite: 'lax',
+      secure: config.isProduction,
+    },
+  })
+);
+
+const { doubleCsrfProtection, generateCsrfToken } = doubleCsrf({
+  getSecret: () => config.sessionSecret,
+  getSessionIdentifier: (req) => req.session.id,
+  cookieName: 'csrf-token',
+  cookieOptions: {
+    sameSite: 'strict',
+    path: '/',
+    secure: config.isProduction,
+    httpOnly: true,
+  },
+  getCsrfTokenFromRequest: (req) => req.body._csrf,
+});
+
+app.use(doubleCsrfProtection);
+app.use((req, res, next) => {
+  res.locals.csrfToken = generateCsrfToken(req, res);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.session.userId
+    ? userModel.findById(req.session.userId) || null
+    : null;
+  next();
+});
+
+app.use(require('./routes/auth'));
+app.use(require('./routes/profile'));
+
 app.get('/', (req, res) => {
   res.render('index');
 });
@@ -22,6 +72,9 @@ app.use((req, res) => {
 
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
+  if (err && err.code === 'EBADCSRFTOKEN') {
+    return res.status(403).render('403');
+  }
   console.error(err);
   res.status(500).render('500');
 });
